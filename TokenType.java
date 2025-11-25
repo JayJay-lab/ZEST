ffed5dc7537dafa179eaacbffb3fccfe6745b67c
JayJay-lab <145923076+JayJay-lab@users.noreply.github.com>
2025-11-25 12:09:55 +0200

    First commit

---
+++ TokenType.java
@@ -0,0 +1,29 @@
+//package com.mycompany.lox;
+
+/**
+ *
+ * @author Admin
+ */
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN , LEFT_BRACE ,RIGHT_BRACE,
+    COMMA ,DOT , MINUS , PLUS , SEMICOLON , SLASH , STAR,
+    
+    //One or two characters tokens
+    BANG, BANG_EQUAL , EQUAL , EQUAL_EQUAL,
+    GREATER , GREATER_EQUAL ,
+    LESS , LESS_THAN,
+    
+    //Literals.
+    IDENTIFIER , STRING , NUMBER,
+    
+    //Keywords
+    AND , CLASS , ELSE , FALSE ,TRUE
+    , FUN , FOR , IF , NIL , OR ,PRINT
+    , RETURN , SUPER , THIS , VAR , WHILE ,
+    EQF
+    
+    
+    
+    
+}
